2a1cdcebd1967b4588f7d81a59ff62043b019acc
Sagiv Mishaan <sagiv505@gmail.com>
2024-11-23 14:29:25 +0200

    vercel update

--- src/server.ts
+++ src/server.ts
@@ -34,3 +34,8 @@ server.listen({ port: PORT }, (err, address) => {
   }
   console.log(`Server listening at ${address}`);
 });
+
+export default async function handler(req: any, res: any) {
+  await server.ready();
+  server.server.emit("request", req, res);
+}
